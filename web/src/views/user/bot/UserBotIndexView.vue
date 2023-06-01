5a0cff001bb12e6fb2f44e80637dc37a9676fe9e
zihui <3335914015@qq.com>
2023-06-01 12:41:20 +0800

    个人页面

--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -1,88 +1,256 @@
 <template>
-    <ContentField>
-        我的个人信息
-    </ContentField>
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <div class="card" style="margin-top: 20px;">
+                    <div class="card-body">
+                        <img :src="$store.state.user.photo" alt="" style="width: 100%;">
+                    </div>
+                </div>
+            </div>
+            <div class="col-9">
+                <div class="card" style="margin-top: 20px;">
+                    <div class="card-header">
+                        <span style="font-size: 125%">我的个人信息</span>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary float-end" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                            创建信息
+                        </button>
+                        <!-- Modal -->
+                        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="staticBackdropLabel">个人信息</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="mb-3">
+                                        <label for="add-bot-title" class="form-label">name</label>
+                                        <input  type="text" class="form-control" id="add-bot-title" placeholder="请输入你的名字">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="add-bot-title" class="form-label">surname</label>
+                                        <input  type="text" class="form-control" id="add-bot-title" placeholder="请输入你的姓氏">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="add-bot-title" class="form-label">sex</label>
+                                        <input  type="text" class="form-control" id="add-bot-title" placeholder="请输入你的性别">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="add-bot-title" class="form-label">address</label>
+                                        <input  type="text" class="form-control" id="add-bot-title" placeholder="请输入你的地址">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="add-bot-title" class="form-label">phone</label>
+                                        <input  type="text" class="form-control" id="add-bot-title" placeholder="请输入电话号">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="add-bot-title" class="form-label">email</label>
+                                        <input  type="email" class="form-control" id="add-bot-title" placeholder="请输入你的email">
+                                    </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">关闭</button>
+                                <button type="button" class="btn btn-primary">保存</button>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-striped table-hover">
+                            <thead>
+                                <tr>
+                                    <th>名称</th>
+                                    <th>操作</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="bot in bots" :key="bot.id">
+                                    <td>{{ bot.title }}</td>
+                                    <td>{{ bot.createtime }}</td>
+                                    <td>
+                                        <button type="button" class="btn btn-secondary" style="margin-right: 10px;" data-bs-toggle="modal" :data-bs-target="'#update-bot-modal-' + bot.id">修改</button>
+                                        <button type="button" class="btn btn-danger" @click="remove_bot(bot)">删除</button>
+
+                                        <div class="modal fade" :id="'update-bot-modal-' + bot.id" tabindex="-1">
+                                            <div class="modal-dialog modal-xl">
+                                                <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title">创建信息</h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <div class="mb-3">
+                                                        <label for="add-bot-title" class="form-label">name</label>
+                                                        <input  type="text" class="form-control" id="add-bot-title" placeholder="请输入Bot名称">
+                                                    </div>
+                                                    <div class="mb-3">
+                                                        <label for="add-bot-title" class="form-label">surname</label>
+                                                        <input  type="text" class="form-control" id="add-bot-title" placeholder="请输入Bot名称">
+                                                    </div>
+                                                    <div class="mb-3">
+                                                        <label for="add-bot-title" class="form-label">sex</label>
+                                                        <input  type="text" class="form-control" id="add-bot-title" placeholder="请输入Bot名称">
+                                                    </div>
+                                                    <div class="mb-3">
+                                                        <label for="add-bot-title" class="form-label">address</label>
+                                                        <input  type="text" class="form-control" id="add-bot-title" placeholder="请输入Bot名称">
+                                                    </div>
+                                                    <div class="mb-3">
+                                                        <label for="add-bot-title" class="form-label">phone</label>
+                                                        <input  type="text" class="form-control" id="add-bot-title" placeholder="请输入Bot名称">
+                                                    </div>
+                                                    <div class="mb-3">
+                                                        <label for="add-bot-title" class="form-label">email</label>
+                                                        <input  type="text" class="form-control" id="add-bot-title" placeholder="请输入Bot名称">
+                                                    </div>
+                                                    <!-- <div class="mb-3">
+                                                        <label for="add-bot-title" class="form-label">名称</label>
+                                                        <input v-model="bot.title" type="text" class="form-control" id="add-bot-title" placeholder="请输入Bot名称">
+                                                    </div> -->
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <div class="error-message">{{ bot.error_message }}</div>
+                                                    <button type="button" class="btn btn-primary" @click="update_bot(bot)">保存</button>
+                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">取消</button>
+                                                </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import ContentField from "@/components/ContentField.vue"
+// import { ref, reactive } from 'vue'
+// import $ from 'jquery'
 // import { useStore } from 'vuex'
-// import $ from 'jquery';
+// import { Modal } from 'bootstrap/dist/js/bootstrap'
+//import ace from 'ace-builds';
+
 export default {
-    components: {
-        ContentField
-    },
     // setup() {
+    //     ace.config.set(
+    //         "basePath", 
+    //         "https://cdn.jsdelivr.net/npm/ace-builds@" + require('ace-builds').version + "/src-noconflict/")
+
     //     const store = useStore();
-    //     $.ajax ({ // add bot
-    //           url: "http://127.0.0.1:3000/user/bot/add/",
-    //           type: "post",
-    //           data: {
-    //             title: "我的bot",
-    //             description: "bot描述",
-    //             content: "代码",
-    //           },
-    //           headers: {
-    //             Authorization: "Bearer " + store.state.user.token,
-    //           },
-    //           success(resp) {
-    //             console.log(resp);
-    //           },
-    //           errot(resp) {
-    //             console.log(resp);
-    //           }
+    //     let bots = ref([]);
+
+    //     const botadd = reactive({
+    //         title: "",
+    //         description: "",
+    //         content: "",
+    //         error_message: "",
     //     });
-    //     $.ajax ({ // 删除bot
-    //           url: "http://127.0.0.1:3000/user/bot/remove/",
-    //           type: "post",
-    //           data: {
-    //             bot_id: 7,
-    //           },
-    //           headers: {
-    //             Authorization: "Bearer " + store.state.user.token,
-    //           },
-    //           success(resp) {
-    //             console.log(resp);
-    //           },
-    //           errot(resp) {
-    //             console.log(resp);
-    //           }
-    //     });
-    //     $.ajax ({ // update bot
-    //           url: "http://127.0.0.1:3000/user/bot/update/",
-    //           type: "post",
-    //           data: {
-    //             bot_id: 4,
-    //             title: "我的bot88",
-    //             description: "bot描述88",
-    //             content: "代码88",
-    //           },
-    //           headers: {
-    //             Authorization: "Bearer " + store.state.user.token,
-    //           },
-    //           success(resp) {
-    //             console.log(resp);
-    //           },
-    //           errot(resp) {
-    //             console.log(resp);
-    //           }
-    //     });
-    //     $.ajax ({ // select bot
-    //           url: "http://127.0.0.1:3000/user/bot/getlist/",
-    //           type: "get",
-    //           headers: {
-    //             Authorization: "Bearer " + store.state.user.token,
-    //           },
-    //           success(resp) {
-    //             console.log(resp);
-    //           },
-    //           errot(resp) {
-    //             console.log(resp);
-    //           }
-    //     });
+
+    //     const refresh_bots = () => {
+    //         $.ajax({
+    //             url: "https://app3657.acapp.acwing.com.cn/api/user/bot/getlist/",
+    //             type: "get",
+    //             headers: {
+    //                 Authorization: "Bearer " + store.state.user.token,
+    //             },
+    //             success(resp) {
+    //                 bots.value = resp;
+    //             }
+    //         })
+    //     }
+
+    //     refresh_bots();
+
+    //     const add_bot = () => {
+    //         botadd.error_message = "";
+    //         $.ajax({
+    //             url: "https://app3657.acapp.acwing.com.cn/api/user/bot/add/",
+    //             type: "post",
+    //             data: {
+    //                 title: botadd.title,
+    //                 description: botadd.description,
+    //                 content: botadd.content,
+    //             },
+    //             headers: {
+    //                 Authorization: "Bearer " + store.state.user.token,
+    //             },
+    //             success(resp) {
+    //                 if (resp.error_message === "success") {
+    //                     botadd.title = "";
+    //                     botadd.description = "";
+    //                     botadd.content = "";
+    //                     Modal.getInstance("#add-bot-btn").hide();
+    //                     refresh_bots();
+    //                 } else {
+    //                     botadd.error_message = resp.error_message;
+    //                 }
+    //             }
+    //         })
+    //     }
+
+    //     const update_bot = (bot) => {
+    //         botadd.error_message = "";
+    //         $.ajax({
+    //             url: "https://app3657.acapp.acwing.com.cn/api/user/bot/update/",
+    //             type: "post",
+    //             data: {
+    //                 bot_id: bot.id,
+    //                 title: bot.title,
+    //                 description: bot.description,
+    //                 content: bot.content,
+    //             },
+    //             headers: {
+    //                 Authorization: "Bearer " + store.state.user.token,
+    //             },
+    //             success(resp) {
+    //                 if (resp.error_message === "success") {
+    //                     Modal.getInstance('#update-bot-modal-' + bot.id).hide();
+    //                     refresh_bots();
+    //                 } else {
+    //                     botadd.error_message = resp.error_message;
+    //                 }
+    //             }
+    //         })
+    //     }
+
+    //     const remove_bot = (bot) => {
+    //         $.ajax({
+    //             url: "https://app3657.acapp.acwing.com.cn/api/user/bot/remove/",
+    //             type: "post",
+    //             data: {
+    //                 bot_id: bot.id,
+    //             },
+    //             headers: {
+    //                 Authorization: "Bearer " + store.state.user.token,
+    //             },
+    //             success(resp) {
+    //                 if (resp.error_message === "success") {
+    //                     refresh_bots();
+    //                 }
+    //             }
+    //         })
+    //     }
+
+    //     return {
+    //         bots,
+    //         botadd,
+    //         add_bot,
+    //         update_bot,
+    //         remove_bot,
+    //     }
     // }
 }
 </script>
 
 <style scoped>
+div.error-message {
+    color: red;
+}
 </style>
---
+++ web/src/views/user/bot/UserBotTestView.vue
@@ -0,0 +1,118 @@
+<template>
+    <ContentField>
+        <form>
+           
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">name</label>
+                <input type="text" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">surname</label>
+                <input type="text" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">sex</label>
+                <input type="text" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">address</label>
+                <input type="text" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">phone</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+            我的个人信息
+    </ContentField>
+</template>
+
+<script>
+import ContentField from "@/components/ContentField.vue"
+// import { useStore } from 'vuex'
+// import $ from 'jquery';
+export default {
+    components: {
+        ContentField
+    },
+    // setup() {
+    //     const store = useStore();
+    //     $.ajax ({ // add bot
+    //           url: "http://127.0.0.1:3000/user/bot/add/",
+    //           type: "post",
+    //           data: {
+    //             title: "我的bot",
+    //             description: "bot描述",
+    //             content: "代码",
+    //           },
+    //           headers: {
+    //             Authorization: "Bearer " + store.state.user.token,
+    //           },
+    //           success(resp) {
+    //             console.log(resp);
+    //           },
+    //           errot(resp) {
+    //             console.log(resp);
+    //           }
+    //     });
+    //     $.ajax ({ // 删除bot
+    //           url: "http://127.0.0.1:3000/user/bot/remove/",
+    //           type: "post",
+    //           data: {
+    //             bot_id: 7,
+    //           },
+    //           headers: {
+    //             Authorization: "Bearer " + store.state.user.token,
+    //           },
+    //           success(resp) {
+    //             console.log(resp);
+    //           },
+    //           errot(resp) {
+    //             console.log(resp);
+    //           }
+    //     });
+    //     $.ajax ({ // update bot
+    //           url: "http://127.0.0.1:3000/user/bot/update/",
+    //           type: "post",
+    //           data: {
+    //             bot_id: 4,
+    //             title: "我的bot88",
+    //             description: "bot描述88",
+    //             content: "代码88",
+    //           },
+    //           headers: {
+    //             Authorization: "Bearer " + store.state.user.token,
+    //           },
+    //           success(resp) {
+    //             console.log(resp);
+    //           },
+    //           errot(resp) {
+    //             console.log(resp);
+    //           }
+    //     });
+    //     $.ajax ({ // select bot
+    //           url: "http://127.0.0.1:3000/user/bot/getlist/",
+    //           type: "get",
+    //           headers: {
+    //             Authorization: "Bearer " + store.state.user.token,
+    //           },
+    //           success(resp) {
+    //             console.log(resp);
+    //           },
+    //           errot(resp) {
+    //             console.log(resp);
+    //           }
+    //     });
+    // }
+}
+</script>
+
+<style scoped>
+</style>
